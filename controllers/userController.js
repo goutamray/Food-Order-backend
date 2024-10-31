495ae0b4f830739179590de8adaa7e371a376dd6
goutam <goutamr362@gmail.com>
2024-10-31 16:57:00 +0600

    user login api completed

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,11 +9,10 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt"; 
 
 
-
 /**
- * @DESC  GET ALL USER
- * @METHOD GET
- * @ROUTE /api/v1/user/create
+ * @DESC  CREATE USER
+ * @METHOD POST
+ * @ROUTE /api/v1/user/signUp
  * @ACCESS PUBLIC 
  * 
  */
@@ -22,7 +21,7 @@ export const createUser = asyncHandler(async(req, res) => {
 
   // validation 
   if (!email || !password) {
-    return res.status(200).json({ message : "All fields are required"}); 
+    return res.status(200).json({ message : "All fields are Required"}); 
   }
 
   try {
@@ -70,5 +69,57 @@ export const createUser = asyncHandler(async(req, res) => {
 
 })
 
+
+/**
+ * @DESC  LOGIN USER
+ * @METHOD POST
+ * @ROUTE /api/v1/user/signIn
+ * @ACCESS PUBLIC 
+ * 
+ */
+export const loginUser = asyncHandler(async(req, res) => {
+  const { email, password } = req.body;
+
+  // validation 
+  if (!email || !password) {
+    return res.status(200).json({ message : "All fields are Required"}); 
+  }
+
+  try {
+    const exisitUser = await User.findOne({ email });
+
+    // check exist user
+    if (!exisitUser) {
+      return res.status(400).json({ message : "Email User Not Found"}); 
+    }
+
+    const matchPassword = await bcrypt.compare(password, exisitUser.password);
+    
+    // check password 
+   if (!matchPassword) {
+      return res.status(400).json({ message : "Password Not Match"}); 
+   }
+
+
+   // create token 
+   const token = jwt.sign(
+    { email : exisitUser.email, id : exisitUser._id},
+     process.env.JSON_WEB_TOKEN_SECRET_KEY
+    )
+
+    // user login
+    return res.status(200).json({ 
+      user : {
+        name : exisitUser?.name,
+        email : exisitUser?.email,
+        photo : exisitUser?.photo,
+      }, 
+      token,  
+      message: "User Login Successfull" });
+
+  } catch (error) {
+    console.log(error.message); 
+  }
+})
 
 
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@
 import express from "express";
 import {
   createUser,
+  loginUser
 } from "../controllers/userController.js";
 import { userPhotoMulter } from "../utilis/multer.js";
 
@@ -11,6 +12,7 @@ const router = express.Router();
 
 // routes 
 router.post("/signUp", userPhotoMulter, createUser); 
+router.post("/signIn", loginUser); 
 
 
 // export default router 
